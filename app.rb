09e33f715b9f1c712aa7212a96634ac25287c4e7
SaiVizag <sspbalusu@outlook.com>
2025-08-11 01:42:05 +0000

    RPS_Project

--- app.rb
+++ app.rb
@@ -1,9 +1,48 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:main_header)
+end
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+  if @comp_move == "rock" then @outcome = "tied"
+  elsif @comp_move == "scissors" then @outcome = "won"
+  else @outcome = "lost"
+  end
+  erb(:rock)
+end
+
+get("/paper") do
+  variant = "paper"
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+  if @comp_move == "rock" then @outcome = "won"
+  elsif @comp_move == "scissors" then @outcome = "lost"
+  else @outcome = "tied"
+  end
+  erb(:paper)
+end
+
+get("/scissors") do
+  variant = "scissors"
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+  if @comp_move == "rock" then @outcome = "lost"
+  elsif @comp_move == "scissors" then @outcome = "tied"
+  else @outcome = "won"
+  end
+  erb(:scissors)
+end
+get("/rules") do
+  erb(:rules)
 end
